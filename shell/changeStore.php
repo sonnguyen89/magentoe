4ebeb0033eccc32d2d21fe551c22bbd83d738f64
Son Nguyen <son.nguyen@balanceinternet.com.au>
2014-05-18 23:04:29 +1000

    update changeStore extension

--- shell/changeStore.php
+++ shell/changeStore.php
@@ -9,30 +9,56 @@ require_once $magentoFilename;
 
 Mage::init();
 
-$storeId = 4;
 
-$products = Mage::getModel('catalog/product')->getCollection()->addStoreFilter($storeId);
+$productNames=array('Nokia 2610 Phone','BlackBerry 8100 Pearl','HTC Touch Diamond ','AT&T 8525 PDA');
+
+
+
+
 $model = Mage::getModel('catalog/product');
+//$storeId = 4;
+//$products = Mage::getModel('catalog/product')->getCollection()->addStoreFilter($storeId);
 	
-		foreach( $products as $product) {
+		/*foreach( $products as $product) {
 			
 		   	$prod=$model->load($product->getId());
 		   	
 		   	$pname = $prod->getName();
-		   	$pstore = $prod->getStoreIds();
+		   	$prodID = $prod->getId();
 		    try {
 		        //$product->save();
-		       $message .=' Product Name: '.$pname.' '.$pstore.'<br/>';
+		       $message .=' Product Name: '.$pname.'-- product id: '.$prodID.'<br/>';
 		      
 		    	
 		    } catch(Exception $e) {
 		        echo $e->getMessage();
 		    }
-		}
-		echo $message;
-		
-		
-		 //Mage::log($message,'products.log');
-		 
+		}*/
+            $productIds = array();
+            foreach($productNames as $prodName)
+            {
+                $prod=$model->loadByAttribute('name', $prodName);
+                $pname = $prod->getName();
+                $prodID = $prod->getId();
+                $message .=' Product Name: '.$pname.'-- product id: '.$prodID.'<br/>';
+                $productIds[] = $prodID;
+
+            }
+            //print_r($productIds);
+            $website_id=2;
+            $websiteRemoveData = array($website_id);
+
+
+            //remove product from website
+            /* @var $actionModel Mage_Catalog_Model_Product_Action */
+            $actionModel = Mage::getSingleton('catalog/product_action');
+
+            $actionModel->updateWebsites($productIds, $websiteRemoveData, 'remove');
+
+//echo $message;
+
+
+ //Mage::log($message,'products.log');
+
 
 
